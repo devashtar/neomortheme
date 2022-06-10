65318992d0f5ddf062f020ef5652217d11beb4be
Михаил <m.prugov@mail.ru>
2022-06-10 16:11:58 +0300

    created a styles folder and changed the styles structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import './global-style.scss'
+
+import './styles/index.scss'
 import { ThemeProvider } from '@context'
 
 import { App } from './App'
